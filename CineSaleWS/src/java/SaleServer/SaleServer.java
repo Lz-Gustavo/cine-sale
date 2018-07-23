3a2a84601b549d92489991808774a5ae66ea65c9
LzGustavo <xavier.luizgustavo@gmail.com>
2018-07-23 15:21:23 -0300

    reserve tickets now working with hashmap tuplespace abstraction and rabbitmq

--- CineSaleWS/src/java/SaleServer/SaleServer.java
+++ CineSaleWS/src/java/SaleServer/SaleServer.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package SaleServer;
 
 import java.text.SimpleDateFormat;
@@ -12,19 +7,23 @@ import javax.jws.WebParam;
 import java.util.*;
 import java.rmi.*;
 
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+
 /**
  * @author GSanM
  * @author lzgustavo
  */
+
 @WebService(serviceName = "SaleServer")
 public class SaleServer {
 	
-	//private List<String> TupleSpace = new ArrayList();
-
-	@WebMethod(operationName = "hello")
-	public String hello(@WebParam(name = "name") String txt) {
-		return "Hello " + txt + " !";
-	}
+	private final Map<String, String> TupleSpace = new HashMap<> ();
+	
+	private final static String PEN_QUEUE = "pendentes";
+	private final static String FIN_QUEUE = "finalizados";
+	private final static String HOST_NAME = "localhost";
 
 	@WebMethod(operationName = "getTime")
 	public String getTime() {
@@ -33,22 +32,6 @@ public class SaleServer {
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
 		return sdf.format(cal.getTime());
-	}
-
-	@WebMethod(operationName = "testRMI")
-	public String testRMI() {
-
-		HelloInterface hello;
-		String name = "rmi://localhost/HelloServer";
-		String text;
-
-		try {
-			hello = (HelloInterface) Naming.lookup(name);
-			text = hello.sayHello();
-			return text;
-		} catch (Exception e) {
-			return ("Hello Client Exception: " + e);
-		}
 	}
 	
 	@WebMethod(operationName = "validate")
@@ -67,20 +50,66 @@ public class SaleServer {
 		}
 	}
 	
+	@WebMethod(operationName = "populate")
+	public void populate() {
+		
+		for (int i = 0; i < 10; i++) {
+			TupleSpace.put("A"+i, "branquelas");
+		}
+	}
+	
 	@WebMethod(operationName = "consulta_espetaculo")
 	public String consulta_estaculo(String name) {
 	// IDEIA: pega todas tuplas que moldam o nome do espetaculo
-	
+		
+		String avaiable = "disponiveis: ";
+
+		if (TupleSpace.containsValue(name)) {
+			for (Map.Entry<String, String> entry : TupleSpace.entrySet()) {
+				
+				if (entry.getValue().equals(name)) {
+					avaiable = avaiable + entry.getKey() + "-";		
+				}
+			}
+		}
+		else {
+			avaiable = ("espetaculo '" + name + "' not found");
+		}
+		return avaiable;
 	}
 	
 	@WebMethod(operationName = "reserva")
-	public String reserva(String tuple) {
+	public String reserva(String tuple) throws Exception {
 	// IDEIA: take do espaco de tuplas, insere na fila de pendentes
+		
+	
+		if (TupleSpace.containsKey(tuple)) {
+
+			ConnectionFactory factory = new ConnectionFactory();
+			factory.setHost(HOST_NAME);
+			Connection connection = factory.newConnection();
+			Channel channel = connection.createChannel();
+			channel.queueDeclare(PEN_QUEUE, false, false, false, null);
+			
+			// remove a tupla do hashmap e publica mensagem de reserva na fila
+			TupleSpace.remove(tuple);
+			String message = "reserva assento " +tuple;
+			channel.basicPublish("", PEN_QUEUE, null, message.getBytes());
+			
+			channel.close();
+			connection.close();
+			
+			return (" [x] Sent '" + message + "'");
+		}
+		else {
+			return ("Assento indisponivel");
+		}
 	}
 	
 	@WebMethod(operationName = "compra")
 	public String compra(String num_cartao) {
 	// IDEIA: se cartao validado, insere na fila de concluidos e retira de 
 	// pendentes, senao, retira de pendentes e re-insere nas tuplas
+		return "teste";
 	}
 }
