2c3fed7c4c1c1d0663d9e48183b6660fd9d77485
Gabriel <gsan.moraes@gmail.com>
2018-07-26 01:37:42 -0300

    Testando RabbitMQ Python

---
+++ rabbitmq_python/testing_rabbit_receiver.py
@@ -0,0 +1,14 @@
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='pendentes')
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+
+channel.basic_consume(callback, queue='pendentes', no_ack=True)    
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
